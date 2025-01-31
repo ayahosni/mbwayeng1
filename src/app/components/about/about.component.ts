e7a045d792a7160942eee85cbd8dcf0a195d9df8
aya <ayahosny4817@gmail.com>
2025-01-31 21:20:32 +0200

    deploy commit

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // استيراد RouterModule
-import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterModule,ContactComponent],
+  imports: [RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
